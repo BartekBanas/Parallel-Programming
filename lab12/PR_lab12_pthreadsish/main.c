374e4a44338c4c26de916fc5c647a77aa414c85a
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-01 14:43:38 +0200

    Refactor

--- lab12/PR_lab12_pthreadsish/main.c
+++ lab12/PR_lab12_pthreadsish/main.c
@@ -7,31 +7,31 @@
 #define M_PI (3.14159265358979323846)
 #endif
 
-int main(void) {
-    int max_liczba_wyrazow = 0;
-
+int main(void)
+{
+    int max_number_of_terms = 0;
     int rank, size;
+
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     if (rank == 0) {
-        max_liczba_wyrazow = 1000;
+        max_number_of_terms = 1000;
     }
 
-    MPI_Bcast(&max_liczba_wyrazow, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&max_number_of_terms, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-// Dekompozycja blokowa: każdy proces oblicza część pętli
-    int block_size = max_liczba_wyrazow / size;
+    int block_size = max_number_of_terms / size;
     int my_start = rank * block_size;
     int my_end = (rank + 1) * block_size - 1;
     int my_stride = 1;
 
-// Ostatni proces oblicza resztę, jeśli max_liczba_wyrazow nie jest podzielne przez size
     if (rank == size - 1) {
-        my_end = max_liczba_wyrazow - 1;
+        my_end = max_number_of_terms - 1;
     }
 
-// Wzór: PI/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 itd. itp.
+// Formula: PI/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 etc.
     double suma_plus = 0.0;
     double suma_minus = 0.0;
     int i = 0;
@@ -42,22 +42,21 @@ int main(void) {
     }
     double pi_approx = 4 * (suma_plus - suma_minus);
 
-// Zgromadź wyniki z każdego procesu na procesie zarządzającym (rank == 0)
     double *results = NULL;
     if (rank == 0) {
         results = (double *) malloc(size * sizeof(double));
     }
+
     MPI_Gather(&pi_approx, 1, MPI_DOUBLE, results, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        // Oblicz sumę wyników z każdego procesu
         double final_result = 0.0;
         for (i = 0; i < size; i++) {
             final_result += results[i];
         }
 
-        printf("PI obliczone: \t\t\t%20.15lf\n", final_result);
-        printf("PI z biblioteki matematycznej: \t%20.15lf\n", M_PI);
+        printf("PI calculated: \t\t\t%20.15lf\n", final_result);
+        printf("PI from the math library: \t%20.15lf\n", M_PI);
         free(results);
     }
 
