8c18b685f9e0a18b2157f76455be87b13562b948
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-01 14:29:39 +0200

    Translation to english

--- lab11/PR_lab11/main.c
+++ lab11/PR_lab11/main.c
@@ -3,18 +3,13 @@
 
 int main(int argc, char **argv) {
 
-    int rank, rankSent, size, source, dest, tag, i;
+    int rank, size, dest, tag, i;
     MPI_Status status;
-
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Print the number of processes
-    //printf("Number of processes: %d\n", num_processes);
-
-    // Pobierz nazwę procesora, na którym dany proces jest uruchomiony
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_length;
     MPI_Get_processor_name(processor_name, &name_length);
@@ -24,9 +19,9 @@ int main(int argc, char **argv) {
         if (rank != 0) {
             dest = 0;
             tag = 0;
-            int lenght = MPI_MAX_PROCESSOR_NAME;
+            int length = MPI_MAX_PROCESSOR_NAME;
 
-            MPI_Send(&lenght, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+            MPI_Send(&length, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
 
             MPI_Send(processor_name, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
 
@@ -35,18 +30,17 @@ int main(int argc, char **argv) {
             for (i = 1; i < size; i++) {
                 int message_in;
                 MPI_Recv(&message_in, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                printf("Dlugosc nazwy do przyjecia: %d\n", message_in);
+                printf("Expected name length: %d\n", message_in);
 
                 MPI_Recv(processor_name, message_in, MPI_CHAR, i,
                          MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                printf("Dane od procesu o randze: %d, uruchomionego na procesorze: %s (status.MPI_SOURCE -> %d) (i=%d)\n",
+                printf("Data from the process with rank: %d, running on the processor: %s (status.MPI_SOURCE -> %d) (i=%d)\n",
                        status.MPI_SOURCE, processor_name, status.MPI_SOURCE, i);
             }
         }
     } else {
-        printf("Pojedynczy proces o randze: %d (brak komunikatów)\n", rank);
+        printf("Single process with rank: %d (no messages)\n", rank);
     }
-
 
     MPI_Finalize();
 
