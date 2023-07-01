35f299219c4e945d4f6275b10547404e137eb2e8
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-01 14:23:39 +0200

    Refactor

--- lab10/PR_lab10_pde/main.c
+++ lab10/PR_lab10_pde/main.c
@@ -3,67 +3,65 @@
 #include<omp.h>
 #include<math.h>
 
-#define N 1000000
+#define SIZE 1000000
 
-int main() {
-
+int main()
+{
     int liczba_watkow = 11;
     omp_set_num_threads(liczba_watkow);
 
     int i;
-    double *A = malloc((N + 2) * sizeof(double));
-    double *B = malloc((N + 2) * sizeof(double));
-    double *C = malloc((N + 2) * sizeof(double));
-    double suma;
+    double *A = malloc((SIZE + 2) * sizeof(double));
+    double *B = malloc((SIZE + 2) * sizeof(double));
+    double *C = malloc((SIZE + 2) * sizeof(double));
+    double sum;
 
-    for (i = 0; i < N + 2; i++) A[i] = (double) i / N;
-    for (i = 0; i < N + 2; i++) B[i] = 1.0 - (double) i / N;
+    for (i = 0; i < SIZE + 2; i++) A[i] = (double) i / SIZE;
+    for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
     double t1 = omp_get_wtime();
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < SIZE; i++) {
         A[i] += A[i + 2] + sin(B[i]);
     }
 
     t1 = omp_get_wtime() - t1;
 
-    suma = 0.0;
-    for (i = 0; i < N + 2; i++) suma += A[i];           // Sequent
-    printf("suma %lf, czas obliczen %lf\n", suma, t1);
+    sum = 0.0;
+    for (i = 0; i < SIZE + 2; i++) sum += A[i];           // Sequent
+    printf("Sum %lf, calculation time %lf\n", sum, t1);
 
+    for (i = 0; i < SIZE + 2; i++) A[i] = (double) i / SIZE;
+    for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
-    for (i = 0; i < N + 2; i++) A[i] = (double) i / N;
-    for (i = 0; i < N + 2; i++) B[i] = 1.0 - (double) i / N;
-
-
-    suma = 0.0;
+    sum = 0.0;
     t1 = omp_get_wtime();
 
-#pragma omp parallel default(none) shared(suma, A, B) num_threads(2)     // Peasant Reduction
-#pragma omp for reduction(+:suma)
-    for (i = 0; i < N; i++) {
-        suma += A[i] + A[i + 2] + sin(B[i]);
+#pragma omp parallel default(none) shared(sum, A, B) num_threads(2)     // Peasant Reduction
+#pragma omp for reduction(+:sum)
+    for (i = 0; i < SIZE; i++) {
+        sum += A[i] + A[i + 2] + sin(B[i]);
     }
     t1 = omp_get_wtime() - t1;
-    suma += A[N] + A[N + 1];
+    sum += A[SIZE] + A[SIZE + 1];
 
-    printf("suma %lf, czas obliczen rownoleglych %lf\n", suma, t1);
+    printf("Sum %lf, parallel computation time %lf\n", sum, t1);
 
-    for (i = 0; i < N + 2; i++) {                                    // Epic Array independence
-        A[i] = (double) i / N;
+    for (i = 0; i < SIZE + 2; i++) {                                    // Epic Array independence
+        A[i] = (double) i / SIZE;
         C[i] = A[i];
     }
-    for (i = 0; i < N + 2; i++) B[i] = 1.0 - (double) i / N;
+    for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
     t1 = omp_get_wtime();
 
 #pragma omp parallel for default(none) shared(A, B, C) num_threads(2)
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < SIZE; i++) {
         A[i] += C[i + 2] + sin(B[i]);
     }
 
     t1 = omp_get_wtime() - t1;
 
-    suma = 0.0;
-    for (i = 0; i < N + 2; i++) suma += A[i];
-    printf("suma %lf, czas obliczen rownoleglych %lf\n", suma, t1);
+    sum = 0.0;
+    for (i = 0; i < SIZE + 2; i++) sum += A[i];
+    printf("Sum %lf, parallel computation time %lf\n", sum, t1);
 }
