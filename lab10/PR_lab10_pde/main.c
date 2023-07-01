2a675d41c1df2a501ede081c39aa181dc245c5b7
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-01 14:25:02 +0200

    Name changes

--- lab10/PR_lab10_pde/main.c
+++ lab10/PR_lab10_pde/main.c
@@ -7,8 +7,8 @@
 
 int main()
 {
-    int liczba_watkow = 11;
-    omp_set_num_threads(liczba_watkow);
+    int threadsAmount = 11;
+    omp_set_num_threads(threadsAmount);
 
     int i;
     double *A = malloc((SIZE + 2) * sizeof(double));
@@ -19,49 +19,52 @@ int main()
     for (i = 0; i < SIZE + 2; i++) A[i] = (double) i / SIZE;
     for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
-    double t1 = omp_get_wtime();
+    double startingTime = omp_get_wtime();
     for (i = 0; i < SIZE; i++) {
         A[i] += A[i + 2] + sin(B[i]);
     }
 
-    t1 = omp_get_wtime() - t1;
+    startingTime = omp_get_wtime() - startingTime;
 
     sum = 0.0;
     for (i = 0; i < SIZE + 2; i++) sum += A[i];           // Sequent
-    printf("Sum %lf, calculation time %lf\n", sum, t1);
+    printf("Sum %lf, calculation time %lf\n", sum, startingTime);
 
     for (i = 0; i < SIZE + 2; i++) A[i] = (double) i / SIZE;
     for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
     sum = 0.0;
-    t1 = omp_get_wtime();
+    startingTime = omp_get_wtime();
 
 #pragma omp parallel default(none) shared(sum, A, B) num_threads(2)     // Peasant Reduction
 #pragma omp for reduction(+:sum)
     for (i = 0; i < SIZE; i++) {
         sum += A[i] + A[i + 2] + sin(B[i]);
     }
-    t1 = omp_get_wtime() - t1;
+
+    startingTime = omp_get_wtime() - startingTime;
     sum += A[SIZE] + A[SIZE + 1];
 
-    printf("Sum %lf, parallel computation time %lf\n", sum, t1);
+    printf("Sum %lf, parallel computation time %lf\n", sum, startingTime);
 
     for (i = 0; i < SIZE + 2; i++) {                                    // Epic Array independence
         A[i] = (double) i / SIZE;
         C[i] = A[i];
     }
+
     for (i = 0; i < SIZE + 2; i++) B[i] = 1.0 - (double) i / SIZE;
 
-    t1 = omp_get_wtime();
+    startingTime = omp_get_wtime();
 
 #pragma omp parallel for default(none) shared(A, B, C) num_threads(2)
     for (i = 0; i < SIZE; i++) {
         A[i] += C[i + 2] + sin(B[i]);
     }
 
-    t1 = omp_get_wtime() - t1;
+    startingTime = omp_get_wtime() - startingTime;
 
     sum = 0.0;
+
     for (i = 0; i < SIZE + 2; i++) sum += A[i];
-    printf("Sum %lf, parallel computation time %lf\n", sum, t1);
+    printf("Sum %lf, parallel computation time %lf\n", sum, startingTime);
 }
