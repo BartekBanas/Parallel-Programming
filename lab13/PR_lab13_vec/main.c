dfbbd44e8f57ea883be2f6ed3145ffcf6c6ee8b5
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-02 23:37:36 +0200

    Reformat and cleanup

--- lab13/PR_lab13_vec/main.c
+++ lab13/PR_lab13_vec/main.c
@@ -10,116 +10,107 @@
 #define DIMENSION 12144
 #define SIZE (DIMENSION*DIMENSION)
 
-void mat_vec(double* a, double* x, double* y, int n, int nt);
+void mat_vec(double *a, double *x, double *y, int n, int nt);
 
-int main ( int argc, char** argv )
+int main(int argc, char **argv)
 {
-    static double x[DIMENSION],y[DIMENSION],z[DIMENSION];
+    static double x[DIMENSION], y[DIMENSION], z[DIMENSION];
     double *a;
-    int n,nt,i,j;
-    const int ione=1;
-    const double done=1.0;
-    const double dzero=0.0;
+    int n, nt, i, j;
 
     double t_begin, t_end;
 
-    int rank, size, source, dest, tag=0;
+    int rank, size, source, dest, tag = 0;
     int row, row_last;
     MPI_Status status;
 
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    n=DIMENSION;
+    n = DIMENSION;
     int width = n;
     int height = n;
 
-    if(rank==0){
+    if (rank == 0)
+    {
+        a = (double *) malloc(SIZE * sizeof(double));
 
-        a = (double *) malloc(SIZE*sizeof(double));
-
-        for(i=0;i<SIZE;i++) a[i]=1.0*i;
-        for(i=0;i<DIMENSION;i++) x[i]=1.0*(DIMENSION-i);
+        for (i = 0; i < SIZE; i++) a[i] = 1.0 * i;
+        for (i = 0; i < DIMENSION; i++) x[i] = 1.0 * (DIMENSION - i);
 
         // for testing - result in y (number of threads switched off)
-        mat_vec(a,x,y,n,4);
+        mat_vec(a, x, y, n, 4);
 
     }
 
     //for(i=0;i<DIMENSION;i++) printf("1 rank %d, x[%2d] = %lf\n",rank,i,x[i]);
 
-    if(size<=1){
-
-
+    if (size <= 1)
+    {
         t_begin = MPI_Wtime();
 
         //#pragma omp parallel for default(none)  shared(n,a,x,z) // num_threads(nt)
-        for(i=0;i<n;i++){
-            double t=0.0;
-            int ni = n*i;
+        for (i = 0; i < n; i++) {
+            double t = 0.0;
+            int ni = n * i;
             int j;
-            for(j=0;j<n;j++){
-                t+=a[ni+j]*x[j];
+            for (j = 0; j < n; j++) {
+                t += a[ni + j] * x[j];
             }
-            z[i]=t; // printf("%lf %lf %lf %lf\n", a[ni],x[0], t, z[i]);
+            z[i] = t; // printf("%lf %lf %lf %lf\n", a[ni],x[0], t, z[i]);
         }
         t_end = MPI_Wtime();
 
         printf("Sequential version\n");
 
-        printf("EXECUTION TIME: executing standard loop: %lf\n", t_end-t_begin);
+        printf("EXECUTION TIME: executing standard loop: %lf\n", t_end - t_begin);
         printf("\tNumber of operations %d, PERFORMANCE %lf GFlops\n",
-               2*width*height, 2*width*height / (t_end-t_begin) * 1e-9);
+               2 * width * height, 2 * width * height / (t_end - t_begin) * 1e-9);
         printf("\tGBytes transferred to processor %lf, speed %lf GB/s\n",
-               2*width*height*sizeof(float)*1e-9,
-               2*width*height*sizeof(float)/(t_end-t_begin)*1e-9);
+               2 * width * height * sizeof(float) * 1e-9,
+               2 * width * height * sizeof(float) / (t_end - t_begin) * 1e-9);
 
-        for(i=0;i<DIMENSION;i++){
-            if(fabs(y[i]-z[i])>1.e-9*z[i]) printf("Error! i = %d: %lf != %lf\n", i, y[i], z[i]);
+        for (i = 0; i < DIMENSION; i++) {
+            if (fabs(y[i] - z[i]) > 1.e-9 * z[i]) printf("Error! i = %d: %lf != %lf\n", i, y[i], z[i]);
         }
 
     }
-
-        /************** || *******************/
-
-    else if(size>1){
-
-        for(i=0;i<DIMENSION;i++) z[i]=0.0;
-        MPI_Bcast(&nt, 1, MPI_INT, 0, MPI_COMM_WORLD );
+    else if (size > 1)
+    {
+        for (i = 0; i < DIMENSION; i++) z[i] = 0.0;
+        MPI_Bcast(&nt, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         // podzial wierszowy
         row = ceil(DIMENSION / size);
         row_last = DIMENSION - row * (size - 1);
 
-        if(row != row_last){
-
+        if (row != row_last)
+        {
             printf("This version does not work with DIMENSION not a multiple of size!\n");
             MPI_Finalize();
             exit(0);
-
         }
 
-        if(rank==0){
-
+        if (rank == 0)
+        {
             //printf("beginning: rank %d size %d\n",rank, size);
 
-            for(i=1;i<size-1;i++){
-                MPI_Send(&a[i * DIMENSION * row], row * DIMENSION, MPI_DOUBLE, i, tag, MPI_COMM_WORLD );
-                MPI_Send(&x[i * row], row, MPI_DOUBLE, i, tag, MPI_COMM_WORLD );
+            for (i = 1; i < size - 1; i++) {
+                MPI_Send(&a[i * DIMENSION * row], row * DIMENSION, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+                MPI_Send(&x[i * row], row, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
             }
 
-            MPI_Send(&a[(size-1) * DIMENSION * row], row_last * DIMENSION, MPI_DOUBLE, size - 1, tag, MPI_COMM_WORLD );
-            MPI_Send(&x[(size-1) * row], row_last, MPI_DOUBLE, size - 1, tag, MPI_COMM_WORLD );
+            MPI_Send(&a[(size - 1) * DIMENSION * row], row_last * DIMENSION, MPI_DOUBLE, size - 1, tag, MPI_COMM_WORLD);
+            MPI_Send(&x[(size - 1) * row], row_last, MPI_DOUBLE, size - 1, tag, MPI_COMM_WORLD);
 
         } else {
-
             //printf("beginning: rank %d size %d\n",rank, size);
 
-            for(i=0;i<DIMENSION;i++) x[i]=0.0;
+            for (i = 0; i < DIMENSION; i++) x[i] = 0.0;
 
             source = 0;
-            if(rank<size-1){
+            if (rank < size - 1) {
 
                 //printf("before aloc, row %d, rank %d size %d\n",row,rank, size);
 
@@ -128,10 +119,9 @@ int main ( int argc, char** argv )
                 //printf("after aloc, row %d, rank %d size %d\n",row,rank, size);
 
                 MPI_Recv(a, row * DIMENSION, MPI_DOUBLE, source,
-                         MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+                         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
                 MPI_Recv(&x[rank * row], row, MPI_DOUBLE, source,
-                         MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-
+                         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             } else {
 
                 //printf("before aloc, row %d, rank %d size %d\n",row_last,rank, size);
@@ -141,73 +131,68 @@ int main ( int argc, char** argv )
                 //printf("after aloc, row %d, rank %d size %d\n",row_last,rank, size);
 
                 MPI_Recv(a, row_last * DIMENSION, MPI_DOUBLE, source,
-                         MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-                MPI_Recv(&x[(size-1) * row], row_last, MPI_DOUBLE, source,
-                         MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-
-
+                         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                MPI_Recv(&x[(size - 1) * row], row_last, MPI_DOUBLE, source,
+                         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             }
 
             //for(i=0;i<DIMENSION;i++) printf("rank %d, received x[%2d] = %lf\n",rank,i,x[i]);
 
-            for(j=0; j < row; j++){
-                for(i=0;i<DIMENSION;i++) {
+            for (j = 0; j < row; j++) {
+                for (i = 0; i < DIMENSION; i++) {
                     //printf("rank %d, received a[%4d] = %lf\n",rank,
                     //     rank*row*DIMENSION+j*DIMENSION+i,a[j*DIMENSION+i]);
                 }
             }
-
         }
 
-        if(rank==0)   t_begin = MPI_Wtime();
-
+        if (rank == 0) t_begin = MPI_Wtime();
 
         //MPI_Allgather(&x[rank*row], row, MPI_DOUBLE, x, row, MPI_DOUBLE, MPI_COMM_WORLD );
-        MPI_Allgather(MPI_IN_PLACE, row, MPI_DOUBLE, x, row, MPI_DOUBLE, MPI_COMM_WORLD );
+        MPI_Allgather(MPI_IN_PLACE, row, MPI_DOUBLE, x, row, MPI_DOUBLE, MPI_COMM_WORLD);
 
         //for(i=0;i<DIMENSION;i++) printf("after Allgather: rank %d, x[%2d] = %lf\n",rank,i,x[i]);
 
         //#pragma omp parallel for default(none)  shared(n, row,a,x,z) // num_threads(nt)
-        for(i=0; i < row; i++){
-            double t=0.0;
-            int ni = n*i;
+        for (i = 0; i < row; i++) {
+            double t = 0.0;
+            int ni = n * i;
             int j;
-            for(j=0;j<n;j++){
-                t+=a[ni+j]*x[j];
+            for (j = 0; j < n; j++) {
+                t += a[ni + j] * x[j];
             }
-            z[i]=t;
+            z[i] = t;
         }
 
         //for(i=0;i<DIMENSION;i++) printf("after mult: rank %d, z[%2d] = %lf\n",rank,i,z[i]);
 
         MPI_Barrier(MPI_COMM_WORLD);
 
-        if(rank==0) {
+        if (rank == 0) {
             t_end = MPI_Wtime();
-            printf("Werja rownolegla MPI/OpenMP\n");
+            printf("Parallel version MPI/OpenMP\n");
 
-            printf("EXECUTION TIME: executing standard loop: %lf\n", t_end-t_begin);
+            printf("EXECUTION TIME: executing standard loop: %lf\n", t_end - t_begin);
             printf("\tNumber of operations %d, PERFORMANCE %lf GFlops\n",
-                   2*width*height, 2*width*height / (t_end-t_begin) * 1e-9);
+                   2 * width * height, 2 * width * height / (t_end - t_begin) * 1e-9);
             printf("\tGBytes transferred to processor %lf, speed %lf GB/s\n",
-                   2*width*height*sizeof(float)*1e-9,
-                   2*width*height*sizeof(float)/(t_end-t_begin)*1e-9);
-
+                   2 * width * height * sizeof(float) * 1e-9,
+                   2 * width * height * sizeof(float) / (t_end - t_begin) * 1e-9);
         }
 
-        if(rank>0){
+        if (rank > 0) {
 
-            MPI_Send(z, row, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD );
+            MPI_Send(z, row, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 
         } else {
 
-            for(i=1;i<size;i++){
-                MPI_Recv(&z[i * row], row, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status  );
+            for (i = 1; i < size; i++) {
+                MPI_Recv(&z[i * row], row, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status);
             }
 
-            for(i=0;i<DIMENSION;i++){
-                if(fabs(y[i]-z[i])>1.e-9*z[i]) {
-                    printf("Blad! i=%d, y[i]=%lf, z[i]=%lf\n",i, y[i], z[i]);
+            for (i = 0; i < DIMENSION; i++) {
+                if (fabs(y[i] - z[i]) > 1.e-9 * z[i]) {
+                    printf("Error! i=%d, y[i]=%lf, z[i]=%lf\n", i, y[i], z[i]);
                 }
             }
         }
@@ -215,7 +200,5 @@ int main ( int argc, char** argv )
 
     MPI_Finalize();
 
-    return(0);
-
-
+    return (0);
 }
